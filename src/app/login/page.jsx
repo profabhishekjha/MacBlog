07d7e8c0138b9dc2d458ab5a67b4ce56fa63f847
ABHISHEK JHA <jhaabhishek910@gmail.com>
2023-09-19 23:55:04 +0530

    auth-fix

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -4,6 +4,7 @@ import styles from "./login.module.css";
 import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+
 const Login = () => {
   const { data, status } = useSession();
   const router = useRouter();
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,18 +1,15 @@
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import GoogleProvider from "next-auth/providers/google"
-import prisma from "./connect"
-import { getServerSession } from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
+import prisma from "./connect";
+import { getServerSession } from "next-auth";
 
-export const authOptions={
-    // Configure one or more authentication providers
-    adapter: PrismaAdapter(prisma),
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      }),
-      // ...add more providers here
-      
-    ],
-  }
-  export const getAuthSessions=()=>getServerSession(authOptions)
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+};
+export const getAuthSessions = () => getServerSession(authOptions);
