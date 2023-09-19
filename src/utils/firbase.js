2ba7013b372b594a82970391ed1fda0a175c7962
ABHISHEK JHA <jhaabhishek910@gmail.com>
2023-09-19 23:15:26 +0530

    fix

--- src/utils/firbase.js
+++ src/utils/firbase.js
@@ -1,19 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.FIREBASE,
-  authDomain: "nextblog-fa810.firebaseapp.com",
-  projectId: "nextblog-fa810",
-  storageBucket: "nextblog-fa810.appspot.com",
-  messagingSenderId: "395665878402",
-  appId: "1:395665878402:web:bd8b3bd90c0d1b74b1f39b",
-  measurementId: "G-M6N1FGT96C"
+  authDomain: "mac-blogs.firebaseapp.com",
+  projectId: "mac-blogs",
+  storageBucket: "mac-blogs.appspot.com",
+  messagingSenderId: "92251596807",
+  appId: "1:92251596807:web:6da786a3d858d2e81aa2a2",
 };
 
 // Initialize Firebase
