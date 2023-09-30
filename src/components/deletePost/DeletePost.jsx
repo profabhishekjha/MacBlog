b5c1d9b3edea4d3dadd4eb8eebe62cac6bc7b807
rainamehak <mehakraina20017@gmail.com>
2023-09-30 20:30:44 +0530

    test

--- src/components/deletePost/DeletePost.jsx
+++ src/components/deletePost/DeletePost.jsx
@@ -14,20 +14,16 @@ const DeleteButton = ({ postId }) => {
 
       if (response.ok) {
         router.push('/')
-        // Post deleted successfully, you can handle this as needed (e.g., show a success message)
         console.log('Post deleted successfully: ', response)
-        // Redirect to the homepage or another appropriate page
       } else {
-        // Error deleting post, handle it accordingly (e.g., show an error message)
         console.error('Error deleting post')
       }
     } catch (error) {
-      // Fetch error, handle it accordingly (e.g., show an error message)
       console.error('Error deleting post:', error)
     }
   }
 
-  return <button onClick={handleDelete}>Delete</button>
+  return null
 }
 
 export default DeleteButton
