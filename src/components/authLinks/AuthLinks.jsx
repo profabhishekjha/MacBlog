9da223bc7b3c265a4e181eba8d83f9853bd5b4d6
rainamehak <mehakraina20017@gmail.com>
2023-11-06 20:01:52 +0530

    update

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 import React, { useContext, useState } from 'react'
 import styles from './authLinks.module.css'
 import Link from 'next/link'
@@ -7,55 +7,98 @@ import { ThemeContext } from '@/context/ThemContext'
 const AuthLinks = () => {
   //temporary
   const { theme } = useContext(ThemeContext)
-  const[open,setOpen]=useState(false)
-  const {status}=useSession()
+  const [open, setOpen] = useState(false)
+  const { status } = useSession()
   return (
     <>
-  {
-    status==="unauthenticated" ?
-    (
-      <Link href="/login" className={styles.link}>Login</Link>
-    ):(
-      <>
-      <Link href="/write" className={styles.link}>Write</Link>
-      <span className={styles.link} onClick={signOut}>Logout</span>
-      </>
-    )
-  }
-  <div className={styles.burger} onClick={()=>setOpen(!open)}>
-    <div className={styles.line} 
-    style={
-      open ? {display:"hidden"}:{ width:"100%" , height:"2px",backgroundColor:theme==="dark"? "white": "black"}
-    }
-    ></div>
-    <div className={styles.line} style={
-      open ? {width:"100%" , height:"2px",backgroundColor:theme==="dark"? "white": "black",rotate:"40deg",translate:"2px 3px"}:{ width:"100%" , height:"2px",backgroundColor:theme==="dark"? "white": "black"}
-    }></div>
-    <div className={styles.line}
-    style={
-      open ? { width:"100%" , height:"2px",backgroundColor:theme==="dark"? "white": "black",rotate:"-44deg",translate:"1px -3px"}:{ width:"100%" , height:"2px",backgroundColor:theme==="dark"? "white": "black"}
-    }
-    ></div>
-  </div>
-  {
-    open &&  (
-      <div className={styles.container} >
-      <Link href="/" onClick={()=>setOpen(!open)}>Homepage</Link>
-      <Link href="/contact" onClick={()=>setOpen(!open)}>Contact</Link>
-      {
-    status==="unauthenticated" ?
-    (
-      <Link href="/login" onClick={()=>setOpen(!open)}>Login</Link>
-    ):(
-      <>
-      <Link href="/write" onClick={()=>setOpen(!open)}>Write</Link>
-      <span className={styles.span} onClick={signOut}>Logout</span>
-      </>
-    )
-  }
+      {status === 'unauthenticated' ? (
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
+      ) : (
+        <>
+          <Link href="/write" className={styles.link}>
+            Write
+          </Link>
+          <span className={styles.link} onClick={signOut}>
+            Logout
+          </span>
+        </>
+      )}
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div
+          className={styles.line}
+          style={
+            open
+              ? { display: 'hidden' }
+              : {
+                  width: '100%',
+                  height: '2px',
+                  backgroundColor: theme === 'dark' ? 'white' : 'black'
+                }
+          }></div>
+        <div
+          className={styles.line}
+          style={
+            open
+              ? {
+                  width: '100%',
+                  height: '2px',
+                  backgroundColor: theme === 'dark' ? 'white' : 'black',
+                  rotate: '40deg',
+                  translate: '2px 3px'
+                }
+              : {
+                  width: '100%',
+                  height: '2px',
+                  backgroundColor: theme === 'dark' ? 'white' : 'black'
+                }
+          }></div>
+        <div
+          className={styles.line}
+          style={
+            open
+              ? {
+                  width: '100%',
+                  height: '2px',
+                  backgroundColor: theme === 'dark' ? 'white' : 'black',
+                  rotate: '-44deg',
+                  translate: '1px -3px'
+                }
+              : {
+                  width: '100%',
+                  height: '2px',
+                  backgroundColor: theme === 'dark' ? 'white' : 'black'
+                }
+          }></div>
       </div>
-    )
-  }
+      {open && (
+        <div className={styles.container}>
+          <Link href="/" onClick={() => setOpen(!open)}>
+            Homepage
+          </Link>
+          <Link href="/contact" onClick={() => setOpen(!open)}>
+            Contact
+          </Link>
+          <Link href="/donate" onClick={() => setOpen(!open)}>
+            Donate
+          </Link>
+          {status === 'unauthenticated' ? (
+            <Link href="/login" onClick={() => setOpen(!open)}>
+              Login
+            </Link>
+          ) : (
+            <>
+              <Link href="/write" onClick={() => setOpen(!open)}>
+                Write
+              </Link>
+              <Link href="/" className={styles.span} onClick={signOut}>
+                Logout
+              </Link>
+            </>
+          )}
+        </div>
+      )}
     </>
   )
 }
