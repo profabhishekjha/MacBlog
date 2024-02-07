6d2f83e81b8a60c6f8dc1112d08a31cc107314e6
ABHISHEK JHA <53990074+profabhishekjha@users.noreply.github.com>
2024-02-07 11:05:26 +0530

    Update firbase.js

--- src/utils/firbase.js
+++ src/utils/firbase.js
@@ -1,15 +1,17 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCgCnI6aEBqMiqus95cjr75BYW930REoCQ',
-  authDomain: 'mac-blogs.firebaseapp.com',
-  projectId: 'mac-blogs',
-  storageBucket: 'mac-blogs.appspot.com',
-  messagingSenderId: '92251596807',
-  appId: '1:92251596807:web:6da786a3d858d2e81aa2a2',
-  measurementId: 'G-GTDW32DNBF'
-}
+  apiKey: "AIzaSyCbuCkkpvRY1i21s9-5pRGiXifJNRFeS_c",
+  authDomain: "new-blog-27874.firebaseapp.com",
+  projectId: "new-blog-27874",
+  storageBucket: "new-blog-27874.appspot.com",
+  messagingSenderId: "372442312519",
+  appId: "1:372442312519:web:11f3ab0618f0abae5f2662"
+};
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
